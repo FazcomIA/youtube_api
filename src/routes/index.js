382b0e6a359da583729c29cc1ce0a11a4cb379f8
Mateus Gomes <dcodecomunidade@gmail.com>
2025-07-07 21:14:43 -0300

    feat: Remover rota /transcript - v1.3.2

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const { searchVideos } = require('../controllers/searchController');
 const { getComments } = require('../controllers/commentsController');
 const { getLatestVideo, getVideoInfo } = require('../controllers/videoController');
-const { getTranscript } = require('../controllers/transcriptController');
 const { healthCheck } = require('../controllers/healthController');
 
 // Rotas da API principais
@@ -13,7 +12,6 @@ router.post('/api/yt_search', searchVideos);
 router.post('/api/comments', getComments);
 router.post('/api/yt_last_video', getLatestVideo);
 router.post('/api/yt_video_info', getVideoInfo);
-router.post('/api/transcript', getTranscript);
 router.get('/health', healthCheck);
 
 module.exports = router; 
